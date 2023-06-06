cdfa061c701069949ccf781c7559bb83d25d457b
Romain TISSERAND <romain.tisserand@gmail.com>
2023-06-06 22:49:09 +0200

    Amlogic A311D2 libmali blobs for Mali-G52 GPU

---
+++ usr/include/EGL/fbdev_window.h
@@ -0,0 +1,10 @@
+/**
+ * @file fbdev_window.h
+ * @brief A window type for the framebuffer device (used by egl and tests)
+ */
+
+#ifndef _FBDEV_WINDOW_H_
+#define _FBDEV_WINDOW_H_
+
+
+#endif
---
+++ usr/include/EGL/wayland_window.h
@@ -0,0 +1,1 @@
+#error "Please don't include this file, wl_egl_window should be opaque for user. the definition should under libMali.so"
